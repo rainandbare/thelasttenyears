45ab5704f2350eb66643e9e7f37ab3f83c1a9ead
Suzette McCanny <suzette@hackeryou.com>
2023-02-03 10:27:32 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// require('dotenv').config()
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.post('/name', (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  console.log('Hellooooooooooooooooo!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
